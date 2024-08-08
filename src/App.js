0a555b9e5091a853b8cbf7253abb8802203e2c78
mohamed-arab1 <momenmoslem991@gmail.com>
2024-08-08 10:52:00 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const respones = axios.get('https://qasimahapp.com/api/categories', {
+      headers: {
+         'Accept-Language': 'ar'
+      }
+
+    })
+    respones.then((response) => console.log(respones))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
